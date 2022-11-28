4de80af6e7d0f9bbe677c918c598dfef4c84de07
MaRtin322s <117728421+MaRtin322s@users.noreply.github.com>
2022-11-28 10:56:20 +0200

    home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <>
+            <center><h1>Welcome to</h1></center>
+            <img src="./images/wink-home.png" className="images" alt="home logo"/>
+            <center><h3>Meet people from all over the world!</h3></center>
+            <center><p>Create a page for a celebrity, brand or business.</p></center>
+        </>
+    );
+}
+
+export default Home;
